6c971f1781371fb9aab29c678228773cff5f87b4
Er <erickruiz1994@gmail.com>
2022-05-19 22:13:48 -0600

    Agregar archivos de proyecto.

---
+++ Solid3/Model/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid3.Model
+{
+    public class Document
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string body { get; set; }
+  
+    }
+}
